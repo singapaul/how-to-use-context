2ac4e2ba8e93cf9a15d3531259352e4d50d8a628
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2022-09-22 09:18:54 +0100

    added all files

---
+++ README.old.md
@@ -0,0 +1,7 @@
+# how-to-use-context
+A repo to show the use context hook. Checkout the different branches for examples 
+
+## branches 
+
+1. Use Context - button example
+2. Use Context - color prop example
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {" "}
+        Link to color button code here:<span> </span>
+        <a href="https://codesandbox.io/s/beautiful-pare-3pqdx5?file=/src/App.tsx">
+          https://codesandbox.io/s/beautiful-pare-3pqdx5?file=/src/App.tsx
+        </a>
+      </div>
+      <div>
+        {" "}
+        Link to form stepper code here:<span> </span>
+        <a href="https://codesandbox.io/s/great-artem-pehfl1?file=/src/FormStepper.tsx">
+          https://codesandbox.io/s/great-artem-pehfl1?file=/src/FormStepper.tsx
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
